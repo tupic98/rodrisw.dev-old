f693f0388e81c45140892eb912f074d64ead3145
tupic98 <raalvarenga423@gmail.com>
2021-06-29 01:15:07 -0600

    :fire: Add current coming soon message

---
+++ components/ComingSoon.vue
@@ -0,0 +1,106 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6 text-center">
+        <h2 class="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+          rodrisw.<span class="text-indigo-600 text-6xl">&#60;</span><span class="text-red-600">/</span>dev<span class="text-indigo-600 text-6xl">&#62;</span>
+        </h2>
+        <h3 class="text-2xl leading-7 font-semibold">
+          Rodrigo Alvarenga
+        </h3>
+        <p class="mt-3 text-gray-600 text-xl">
+          Software Developer
+        </p>
+        <p class="mt-3 text-gray-600">
+          Welcome to my site! I'm a growing software developer soon to be software engineer. Looking forward to the making of this site to prove my skills, start learning about emerging technologies and about web development. Also plannig to do a blog to document all my journey.
+        </p>
+        <p class="mt-p4 pt-4 text-gray-800 border-t border-dashed">
+          You can start following me in the next links:
+        </p>
+      </div>
+      <div class="flex justify-center items-center pt-4 space-x-4">
+        <a href="https://github.com/tupic98" target="_blank"><svg
+          class="w-10 h-10 text-gray-600 hover:text-gray-800"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          width="32"
+          height="32"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+        ><path d="M12 2.247a10 10 0 0 0-3.162 19.487c.5.088.687-.212.687-.475c0-.237-.012-1.025-.012-1.862c-2.513.462-3.163-.613-3.363-1.175a3.636 3.636 0 0 0-1.025-1.413c-.35-.187-.85-.65-.013-.662a2.001 2.001 0 0 1 1.538 1.025a2.137 2.137 0 0 0 2.912.825a2.104 2.104 0 0 1 .638-1.338c-2.225-.25-4.55-1.112-4.55-4.937a3.892 3.892 0 0 1 1.025-2.688a3.594 3.594 0 0 1 .1-2.65s.837-.262 2.75 1.025a9.427 9.427 0 0 1 5 0c1.912-1.3 2.75-1.025 2.75-1.025a3.593 3.593 0 0 1 .1 2.65a3.869 3.869 0 0 1 1.025 2.688c0 3.837-2.338 4.687-4.563 4.937a2.368 2.368 0 0 1 .675 1.85c0 1.338-.012 2.413-.012 2.75c0 .263.187.575.687.475A10.005 10.005 0 0 0 12 2.247z" fill="currentColor" /></svg></a>
+        <a href="https://twitter.com/AlejandroTupic" target="_blank"><svg
+          class="w-10 h-10 text-gray-600 hover:text-gray-800"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          width="32"
+          height="32"
+          preserveAspectRatio="xMidYMid meet"
+          viewBox="0 0 24 24"
+        ><path d="M22.46 6c-.77.35-1.6.58-2.46.69c.88-.53 1.56-1.37 1.88-2.38c-.83.5-1.75.85-2.72 1.05C18.37 4.5 17.26 4 16 4c-2.35 0-4.27 1.92-4.27 4.29c0 .34.04.67.11.98C8.28 9.09 5.11 7.38 3 4.79c-.37.63-.58 1.37-.58 2.15c0 1.49.75 2.81 1.91 3.56c-.71 0-1.37-.2-1.95-.5v.03c0 2.08 1.48 3.82 3.44 4.21a4.22 4.22 0 0 1-1.93.07a4.28 4.28 0 0 0 4 2.98a8.521 8.521 0 0 1-5.33 1.84c-.34 0-.68-.02-1.02-.06C3.44 20.29 5.7 21 8.12 21C16 21 20.33 14.46 20.33 8.79c0-.19 0-.37-.01-.56c.84-.6 1.56-1.36 2.14-2.23z" fill="currentColor" /></svg></a>
+        <a href="https://www.polywork.com/raalvarenga" target="_blank" class="text-gray-600 hover:text-gray-800 text-2xl font-bold">
+          Polywork
+        </a>
+        <a href="mailto:raalvarenga@rodrisw.dev" target="_blank">
+          <svg
+            class="w-10 h-10 text-gray-600 hover:text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            aria-hidden="true"
+            role="img"
+            width="32"
+            height="32"
+            preserveAspectRatio="xMidYMid meet"
+            viewBox="0 0 24 24"
+          ><path d="m21.5 4h-19c-1.378 0-2.5 1.122-2.5 2.5v11c0 1.378 1.122 2.5 2.5 2.5h19c1.378 0 2.5-1.122 2.5-2.5v-11c0-1.378-1.122-2.5-2.5-2.5zm.5 5.41-8.61 4.5c-.43.22-.91.34-1.39.34s-.95-.12-1.39-.34l-8.61-4.5v-2.26l9.53 4.98c.29.15.65.15.94 0l9.53-4.98z" fill="currentColor" /></svg>
+        </a>
+        <a href="https://www.linkedin.com/in/rodrigo-alvarenga-928b7a190/" target="_blank">
+          <svg
+            id="Capa_1"
+            class="w-8 h-8 text-gray-600 hover:text-gray-800"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            x="0px"
+            y="0px"
+            width="510px"
+            height="510px"
+            viewBox="0 0 510 510"
+            style="enable-background:new 0 0 510 510;"
+            xml:space="preserve"
+          >
+            <g>
+              <g id="post-linkedin">
+                <path
+                  d="M459,0H51C22.95,0,0,22.95,0,51v408c0,28.05,22.95,51,51,51h408c28.05,0,51-22.95,51-51V51C510,22.95,487.05,0,459,0z M153,433.5H76.5V204H153V433.5z M114.75,160.65c-25.5,0-45.9-20.4-45.9-45.9s20.4-45.9,45.9-45.9s45.9,20.4,45.9,45.9 S140.25,160.65,114.75,160.65z M433.5,433.5H357V298.35c0-20.399-17.85-38.25-38.25-38.25s-38.25,17.851-38.25,38.25V433.5H204 V204h76.5v30.6c12.75-20.4,40.8-35.7,63.75-35.7c48.45,0,89.25,40.8,89.25,89.25V433.5z"
+                  fill="currentColor"
+                />
+              </g>
+            </g>
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+            <g />
+          </svg>
+
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-gray-100">
+    <Nuxt />
+  </div>
+</template>
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator';
+
+@Component
+export default class Default extends Vue {}
+
+</script>
